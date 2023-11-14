928f55d4dc3cbf383ce2a0b71210c9b7bbab9fab
itsmeNanda <nanda.bagus2006@gmail.com>
2023-11-14 21:05:21 +0700

    express-basic

---
+++ controller/user.js
@@ -0,0 +1,64 @@
+let users = [
+    { id: 1, nama: "Nanda", email: "nanda@gmail.com" },
+    { id: 2, nama: "Bagus", email: "Bagus@gmail.com" },
+]
+
+module.exports = {
+    index: (req, res) => {
+        if (users.length > 0) {
+            res.json({
+                status: true,
+                data: users,
+                method: req.method,
+                url: req.url
+            })
+        } else {
+            res.json({
+                status: false,
+                message: "Data masih kosong"
+            })
+        }
+    },
+
+    store: (req, res) => {
+        users.push(req.body)
+        res.json({
+            status: true,
+            data: users,
+            method: req.method,
+            url: req.url,
+            message: "Data berhasil di tambahkan"
+        })
+    },
+
+    ubah: (req, res) => {
+        const id = req.params.id
+        users.filter(user => {
+            if (user.id == id) {
+                user.nama = req.body.nama
+                user.email = req.body.email
+                return user
+            }
+        })
+        res.json({
+            status: true,
+            data: users,
+            method: req.method,
+            url: req.url,
+            message: "Data berhasil di ubah"
+        })
+    },
+
+    hapus: (req, res) => {
+        const id = req.params.id
+        users = users.filter(user => user.id != id)
+
+        res.json({
+            status: true,
+            data: users,
+            method: req.method,
+            url: req.url,
+            message: "Data berhasil di hapus"
+        })
+    },
+}
